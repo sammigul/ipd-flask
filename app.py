36839ce4d0932dd106fa1de07ba2c8c42af9239b
Abdul Sammi Gul <sammigul11005@gmail.com>
2023-11-29 15:08:03 +0500

    Fixed Cors for measure_ipd route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS, cross_origin
 import cv2
 import cv2.aruco as aruco
 import os
@@ -8,16 +9,18 @@ from utils import is_valid_image
 
 app = Flask(__name__)
 
+
 # Constants for ArUco marker size and dictionary type
 MARKER_SIZE_MM = 75
 ARUCO_DICT_TYPE = aruco.DICT_4X4_100
 
 
 @app.route('/measure_ipd',methods=['POST'])
+@cross_origin()
 def measure_ipd():
     try:
         # user_image = request.files['userImage']
-        user_image = request.files.get('userImage') 
+        user_image = request.files.get('userImage')
         if user_image is None or not user_image.filename: # if no image is provided or the image is empty
             return jsonify({'error': 'No image provided'}), 400
         if (not is_valid_image(user_image)): 
@@ -36,6 +39,7 @@ def measure_ipd():
 
         result = imageAnalysis(saved_filename,MARKER_SIZE_MM,ARUCO_DICT_TYPE)
         os.remove(saved_filename)
+        print(result)
         if (type(result) == str):
             return jsonify({'error': result}), 400 # bad image provided by user appropriate error code is returned
         else:
@@ -43,7 +47,8 @@ def measure_ipd():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# filename = '75mm.pdf'
 
-
-
-
+@app.route('/aruco_marker',methods=['GET'])
+def serve_pdf():
+    return send_from_directory('static', '75mm.pdf')
